47b756926634c43e29872c9508d01b1c6b70b14d
Daniel Teal <daniel@teals.org>
2015-11-05 14:06:58 -0600

    Start GUI

---
+++ src/gui/FractalPanel.java
@@ -0,0 +1,16 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class FractalPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public FractalPanel(){
+		setPreferredSize(new Dimension(500, 500));
+		setBackground(Color.blue);
+	}
+	
+}
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -4,8 +4,22 @@ import javax.swing.JFrame;
 
 public class Main extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
+	FractalPanel fp = new FractalPanel();
+	
+	public Main(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		add(fp);
+		pack();
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		new Main();
 	}
 
 }
