327ce4dac250ff9121e75fe3edcdfee047e1ef0b
Daniel Teal <daniel@teals.org>
2015-11-05 14:12:31 -0600

    GUI Hello World

--- src/gui/FractalPanel.java
+++ src/gui/FractalPanel.java
@@ -2,6 +2,8 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class FractalPanel extends JPanel {
@@ -10,7 +12,14 @@ public class FractalPanel extends JPanel {
 
 	public FractalPanel(){
 		setPreferredSize(new Dimension(500, 500));
-		setBackground(Color.blue);
+		setBackground(Color.lightGray);
+	}
+	
+	public void paint(Graphics g){
+		super.paint(g);
+		g.setColor(Color.black);
+		g.setFont(new Font("Verdana", Font.PLAIN, 50)); 
+		g.drawString("Hello, world!", 10, 50);
 	}
 	
 }
