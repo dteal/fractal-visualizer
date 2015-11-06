26ebd482db36acf4c3f3ac85693bccf520398593
Daniel Teal <daniel@teals.org>
2015-11-06 08:32:50 -0600

    Started adding buffering.

--- src/gui/FractalPanel.java
+++ src/gui/FractalPanel.java
@@ -4,12 +4,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JPanel;
 
 public class FractalPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
+	BufferedImage image;
+	
 	public FractalPanel(){
 		setPreferredSize(new Dimension(500, 500));
 		setBackground(Color.lightGray);
@@ -20,6 +24,21 @@ public class FractalPanel extends JPanel {
 		g.setColor(Color.black);
 		g.setFont(new Font("Verdana", Font.PLAIN, 50)); 
 		g.drawString("Hello, world!", 10, 50);
+	}
+	
+	public void renderImage(){
+		image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
+		double top = 2;
+		double bottom = -2;
+		double left = -2;
+		double right = 2;
+		double width = 500;
+		double height = 500;
+		for(int x_idx = 0; x_idx < width; x_idx++){
+			for(int y_idx = 0; y_idx < height; y_idx++){
+				
+			}
+		}
 	}
 	
 }
