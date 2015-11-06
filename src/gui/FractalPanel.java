6f6dbf8ac2a5f7c764801e6f38d6e1d43ffe0e1a
Daniel Teal <daniel@teals.org>
2015-11-06 12:45:05 -0600

    Add escape time algorithm.

--- src/gui/FractalPanel.java
+++ src/gui/FractalPanel.java
@@ -16,29 +16,62 @@ public class FractalPanel extends JPanel {
 	
 	public FractalPanel(){
 		setPreferredSize(new Dimension(500, 500));
-		setBackground(Color.lightGray);
+		//setBackground(Color.lightGray);
+		//System.out.println(inMandelbrot(5,0));
+		renderImage();
 	}
 	
 	public void paint(Graphics g){
 		super.paint(g);
-		g.setColor(Color.black);
-		g.setFont(new Font("Verdana", Font.PLAIN, 50)); 
-		g.drawString("Hello, world!", 10, 50);
+		//g.setColor(Color.black);
+		//g.setFont(new Font("Verdana", Font.PLAIN, 50)); 
+		//g.drawString("Hello, world!", 10, 50);
+		g.drawImage(image, 0, 0, null);
 	}
 	
 	public void renderImage(){
-		image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
-		double top = 2;
-		double bottom = -2;
-		double left = -2;
-		double right = 2;
-		double width = 500;
-		double height = 500;
+		double top = 2; // Top value in complex plane
+		double bottom = -2; // Bottom value in complex plane
+		double left = -2; // Left value in complex plane
+		double right = 2; // Right value in complex plane
+		int width = (int)getSize().getWidth(); // Width of image
+		int height = (int)getSize().getHeight(); // Height of image
+		if(width <= 0 || height <= 0){
+			image = null;
+			return;
+		}
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		for(int x_idx = 0; x_idx < width; x_idx++){
 			for(int y_idx = 0; y_idx < height; y_idx++){
-				
+				// The x-coordinate in the complex plane
+				double x = (right-left)*x_idx/(width-1)+left;
+				// The y-coordinate in the complex plane
+				double y = (top-bottom)*(height-y_idx-1)/(height-1)+bottom;
+				// System.out.println(x + " " + y);
+				image.setRGB(x_idx, y_idx, new Color(0,0,inMandelbrot(x,y)*10).getRGB());
 			}
 		}
 	}
 	
+	public int inMandelbrot(double x, double y){
+		int MAX_ITERATIONS = 20;
+		
+		double px = 0;
+		double py = 0;
+		int iterations = 0;
+		while(square(px)+square(py)<2*2 && iterations < MAX_ITERATIONS){
+			double nx = square(px)-square(py)+x;
+			double ny = 2*px*py + y;
+			iterations ++;
+			px = nx;
+			py = ny;
+		}
+		
+		// Return 0 if in Mandelbrot, 1 if one iteration away from being inside, etc.
+		return MAX_ITERATIONS - iterations;
+	}
+	
+	public double square(double n){
+		return n * n;
+	}
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,25 +1,68 @@
 package gui;
 
-import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
-public class Main extends JFrame {
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+
+public class Main extends JFrame implements ComponentListener{
 
 	private static final long serialVersionUID = 1L;
 
 	FractalPanel fp = new FractalPanel();
+
+	// Declare menu bar items
+
+	JMenuBar menubar = new JMenuBar();
+	JMenu file = new JMenu("File");
+	JMenuItem file_exit = new JMenuItem("Exit");
+	JMenu view = new JMenu("View");
+	JCheckBoxMenuItem view_grid = new JCheckBoxMenuItem("Zoom");
+	JMenu help = new JMenu("Help");
+	JMenuItem help_about = new JMenuItem("About Fractal Visualizer");
 	
 	public Main(){
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+		setTitle("Fractal Visualizer");
+		setMinimumSize(new Dimension(200,200));
+		
+		fp.addComponentListener(this);
 		add(fp);
 		pack();
-		
+		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+		}
+		
 		new Main();
+	}
+
+	public void componentHidden(ComponentEvent e) {
+		
+	}
+
+	public void componentMoved(ComponentEvent e) {
+		
+	}
+
+	public void componentResized(ComponentEvent e) {
+		fp.renderImage();
+	}
+
+	public void componentShown(ComponentEvent e) {
+		
 	}
 
 }
